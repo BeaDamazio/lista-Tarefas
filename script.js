f55782432ec24830a7cb0674ffdc7d642f7e676a
Bea Damazio <bia.franco@live.com>
2023-10-02 22:09:45 -0300

    style: att de style

--- script.js
+++ script.js
@@ -5,18 +5,27 @@ let listaPai = document.querySelector('.lista-pai');
 button.addEventListener('click', () => {
     let lista = document.createElement('li')
     let checkButton = document.createElement('button')
+    let trashButton = document.createElement('button')
 
     checkButton.addEventListener('click', () => {
         lista.classList.add('risca-texto');
+    })
+
+    trashButton.addEventListener('click', () => {
+        lista.remove();
     })
 
     lista.setAttribute('class', 'lista');
     lista.innerText = input.value;
     listaPai.append(lista);
     let imgCheck = document.createElement('img');
+    let imgTrash = document.createElement('img');
     imgCheck.setAttribute('src', './img/check.png');
+    imgTrash.setAttribute('src', './img/trash.png');
     checkButton.append(imgCheck);
+    trashButton.append(imgTrash);
     lista.append(checkButton);
+    lista.append(trashButton);
     input.value = '';
 
 });
